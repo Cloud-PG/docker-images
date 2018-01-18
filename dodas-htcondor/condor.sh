aa22d422b113e22456874f0876498040d5d03bab
MircoT <mirco.theone@gmail.com>
2018-01-18 15:44:48 +0100

    Fix condor configuration

--- dodas-htcondor/condor.sh
+++ dodas-htcondor/condor.sh
@@ -2,16 +2,18 @@
 
 if [ "$1" == "master" ];
 then
-    echo "==> Copy configuration file for master node"
-    j2 /opt/dodas/htc_config/condor_config.template /opt/dodas/htc_config/master_condor_config.json > /etc/condor/condor_config
+    echo "==> Compile configuration file for master node with env vars"
+    export CONDOR_DAEMON_LIST="COLLECTOR, MASTER, NEGOTIATOR"
+    j2 /opt/dodas/htc_config/condor_config.template > /etc/condor/condor_config
     echo "==> Start condor"
     condor_master
     echo "==> Start service"
     python -m SimpleHTTPServer 5000
 elif [ "$1" == "wn" ];
 then
-    echo "==> Copy configuration file for worker node"
-    j2 /opt/dodas/htc_config/condor_config.template /opt/dodas/htc_config/wn_condor_config.json > /etc/condor/condor_config
+    echo "==> Compile configuration file for worker node with env vars"
+    export CONDOR_DAEMON_LIST="MASTER, STARTD"
+    j2 /opt/dodas/htc_config/condor_config.template > /etc/condor/condor_config
     echo "==> Start condor"
     condor_master
     echo "==> Start service"
@@ -19,10 +21,22 @@ then
     python -m SimpleHTTPServer $RAND_PORT
 elif [ "$1" == "schedd" ];
 then
-    echo "==> Copy configuration file for sheduler node"
-    j2 /opt/dodas/htc_config/condor_config.template /opt/dodas/htc_config/schedd_condor_config.json > /etc/condor/condor_config
+    echo "==> Compile configuration file for sheduler node with env vars"
+    export CONDOR_DAEMON_LIST="MASTER, SCHEDD"
+    j2 /opt/dodas/htc_config/condor_config.template > /etc/condor/condor_config
     echo "==> Start condor"
     condor_master
-    echo "==> Start sshd on port $2"
-    exec /usr/sbin/sshd -E /var/log/sshd.log -g 30 -p $2 -D
+    echo "==> Start sshd on port $CONDOR_SCHEDD_SSH_PORT"
+    exec /usr/sbin/sshd -E /var/log/sshd.log -g 30 -p $CONDOR_SCHEDD_SSH_PORT -D
+elif [ "$1" == "all" ];
+then
+    echo "==> Compile configuration file for sheduler node with env vars"
+    j2 /opt/dodas/htc_config/condor_config.template > /etc/condor/condor_config
+    echo "==> Start condor"
+    condor_master
+    echo "==> Start sshd on port $CONDOR_SCHEDD_SSH_PORT"
+    exec /usr/sbin/sshd -E /var/log/sshd.log -g 30 -p $CONDOR_SCHEDD_SSH_PORT -D
+else
+    echo "[ERROR]==> You have to supply a role, like: 'master', 'wn', 'schedd' or 'all'..."
+    exit 1
 fi
