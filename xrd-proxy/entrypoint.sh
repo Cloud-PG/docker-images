202aba635750221f6295fcc2c315ce568531584c
dciangot <diego.ciangottini@cern.ch>
2018-01-30 15:07:59 +0100

    v1.1

--- xrd-proxy/entrypoint.sh
+++ xrd-proxy/entrypoint.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
+if [[ -z "$1" ]]; then
+    if [[ "$1" -eq "proxy" ]]; then
+
+        cp /etc/xrootd/xrd_cache.conf  /etc/xrootd/xrd.conf
+        if [[ "$2" -eq "-redirector_url" && -z "$3" ]]; then
+            sed -i -e "s/rdtr_cache/$3/" /etc/xrootd/xrd.conf
+        else if [[ "$2" -eq "-cmsd_redirector_port" && -z "$3" ]]; then
+            sed -i -e "s/rdtr_port_cmsd/$3/" /etc/xrootd/xrd.conf
+        else if [[ "$2" -eq "-xrd_redirector_port" && -z "$3" ]]; then
+            sed -i -e "s/rdtr_port_xrd/$3/" /etc/xrootd/xrd.conf
+        else
+            echo "Invalid options"    
+        fi
+
+    else if [[ "$1" -eq "redirector" ]]; then
+        cp /etc/xrootd/xrd_redirector.conf  /etc/xrootd/xrd.conf
+    else 
+        echo "no Valid options"
+    fi
+
+fi
+
 exec xrootd -c /etc/xrootd/xrd.conf 
