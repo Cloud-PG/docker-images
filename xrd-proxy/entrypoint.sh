8af1f2d398202a4109bab5e2c26a64dc90b4e596
dciangot <diego.ciangottini@cern.ch>
2018-01-30 14:40:28 +0000

    fix cmsd

--- xrd-proxy/entrypoint.sh
+++ xrd-proxy/entrypoint.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-if [[ -z "$1" ]]; then
+echo $@
+
+if [[ -n "$1" ]]; then
+    echo "HIIII"
     if [[ "$1" -eq "proxy" ]]; then
+    echo "HIIII"
 
         cp /etc/xrootd/xrd_cache.conf  /etc/xrootd/xrd.conf
-        if [[ "$2" -eq "-redirector_url" && -z "$3" ]]; then
+        if [[ "$2" -eq "-redirector_url" && -n "$3" ]]; then
+    echo "HIIII"
             sed -i -e "s/rdtr_cache/$3/" /etc/xrootd/xrd.conf
-        else if [[ "$2" -eq "-cmsd_redirector_port" && -z "$3" ]]; then
-            sed -i -e "s/rdtr_port_cmsd/$3/" /etc/xrootd/xrd.conf
-        else if [[ "$2" -eq "-xrd_redirector_port" && -z "$3" ]]; then
-            sed -i -e "s/rdtr_port_xrd/$3/" /etc/xrootd/xrd.conf
-        else
-            echo "Invalid options"    
+        fi
+        if [[ "$4" -eq "-cmsd_redirector_port" && -n "$5" ]]; then
+            sed -i -e "s/rdtr_port_cmsd/$5/" /etc/xrootd/xrd.conf
+        fi
+        if [[ "$6" -eq "-xrd_redirector_port" && -n "$7" ]]; then
+            sed -i -e "s/rdtr_port_xrd/$7/" /etc/xrootd/xrd.conf
         fi
 
-    else if [[ "$1" -eq "redirector" ]]; then
+    elif [[ "$1" -eq "redirector" ]]; then
         cp /etc/xrootd/xrd_redirector.conf  /etc/xrootd/xrd.conf
     else 
         echo "no Valid options"
@@ -22,4 +27,5 @@ if [[ -z "$1" ]]; then
 
 fi
 
-exec xrootd -c /etc/xrootd/xrd.conf 
+xrootd -b -c /etc/xrootd/xrd.conf -l /var/log/xrootd/proxyXrd.log 
+exec cmsd -c /etc/xrootd/xrd.conf
