d9f5de1ac47fa1b33b2aa59e6d32901bb4d3c6e2
dciangot <diego.ciangottini@cern.ch>
2018-01-31 11:16:45 +0100

    fix redirector

--- xrd-proxy/entrypoint.sh
+++ xrd-proxy/entrypoint.sh
@@ -3,32 +3,32 @@
 echo $@
 
 if [[ -n "$1" ]]; then
-    if [[ "$1" -eq "proxy" ]]; then
+    if [[ "$1" == "proxy" ]]; then
 
         cp /etc/xrootd/xrd_cache.conf  /etc/xrootd/xrd.conf
-        if [[ "$2" -eq "-redirector_url" && -n "$3" ]]; then
+        if [[ "$2" == "-redirector_url" && -n "$3" ]]; then
             sed -i -e "s/rdtr_cache/$3/" /etc/xrootd/xrd.conf
         fi
-        if [[ "$4" -eq "-cmsd_redirector_port" && -n "$5" ]]; then
+        if [[ "$4" == "-cmsd_redirector_port" && -n "$5" ]]; then
             sed -i -e "s/rdtr_port_cmsd/$5/" /etc/xrootd/xrd.conf
         fi
-        if [[ "$6" -eq "-xrd_redirector_port" && -n "$7" ]]; then
+        if [[ "$6" == "-xrd_redirector_port" && -n "$7" ]]; then
             sed -i -e "s/rdtr_port_xrd/$7/" /etc/xrootd/xrd.conf
         fi
 
-    elif [[ "$1" -eq "redirector" ]]; then
+    elif [[ "$1" == "redirector" ]]; then
         cp /etc/xrootd/xrd_redirector.conf  /etc/xrootd/xrd.conf
-        if [[ "$2" -eq "-cmsd_redirector_port" && -n "$3" ]]; then
+        if [[ "$2" == "-cmsd_redirector_port" && -n "$3" ]]; then
             sed -i -e "s/rdtr_port_cmsd/$3/" /etc/xrootd/xrd.conf
         fi
-        if [[ "$4" -eq "-xrd_redirector_port" && -n "$5" ]]; then
+        if [[ "$4" == "-xrd_redirector_port" && -n "$5" ]]; then
             sed -i -e "s/rdtr_port_xrd/$5/" /etc/xrootd/xrd.conf
         fi
-        if [[ "$6" -eq "-redirector_global" && -n "$7" ]]; then
-            sed -i -e "s/rdtr_global_port/$7/" /etc/xrootd/xrd.conf
+        if [[ "$6" == "-redirector_global" && -n "$7" ]]; then
+            sed -i -e "s/rdtr_global:/$7/" /etc/xrootd/xrd.conf
         fi
-        if [[ "$6" -eq "-redirector_global_port" && -n "$7" ]]; then
-            sed -i -e "s/rdtr_global/$7/" /etc/xrootd/xrd.conf
+        if [[ "$8" == "-redirector_global_port" && -n "$9" ]]; then
+            sed -i -e "s/rdtr_global_port/$9/" /etc/xrootd/xrd.conf
         fi 
     else 
         echo "no Valid options"
