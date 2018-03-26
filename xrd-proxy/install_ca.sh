4ff358b8d7b764928a247ed4eb896c8a8f824a2d
dciangot <diego.ciangottini@cern.ch>
2018-03-26 19:11:53 +0200

    dciangot

--- xrd-proxy/install_ca.sh
+++ xrd-proxy/install_ca.sh
@@ -8,5 +8,7 @@ yum -y install ca_CMS-TTS-CA
 
 chmod 600 /etc/grid-security/xrd/userkey.pem
 
+sudo chown -R xrootd:xrootd /etc/grid-security/xrd/
+
 echo "Retrieving proxy"
 sudo -u xrootd grid-proxy-init -cert /etc/grid-security/xrd/usercert.pem -key /etc/grid-security/xrd/userkey.pem
