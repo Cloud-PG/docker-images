260e07de3a5ef93cbc31e2b72a003ce2147b39b6
MircoT <mirco.theone@gmail.com>
2018-04-18 09:18:50 +0200

    Fix check condor master ip

--- dodas-htcondor/check_condor_master_ip.sh
+++ dodas-htcondor/check_condor_master_ip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ZOOKEEPER_CONDOR_HOST=$(dodas_cache --wait-for true zookeeper CONDOR_HOST || echo "None")
+CONDOR_HOST=$(condor_config_val CONDOR_HOST | sed -e 's/^[ \t\n]*//')
 
 if [ "$CONDOR_HOST" == "$ZOOKEEPER_CONDOR_HOST" ];
 then
