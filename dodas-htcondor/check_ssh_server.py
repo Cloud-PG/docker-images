22c6de597a21b80ada6264474db62101c55dcb29
MircoT <mirco.theone@gmail.com>
2018-05-09 10:29:12 +0200

    fix check ssh port in condor

--- dodas-htcondor/check_ssh_server.py
+++ dodas-htcondor/check_ssh_server.py
@@ -2,13 +2,14 @@
 from paramiko import (AuthenticationException, AutoAddPolicy,
                       BadHostKeyException, SSHException)
 from paramiko.client import SSHClient
+from os import environ
 
 
 def main():
     try:
         client = SSHClient()
         client.set_missing_host_key_policy(AutoAddPolicy)
-        client.connect("localhost", port=31042,
+        client.connect("localhost", port=environ.get('CONDOR_SCHEDD_SSH_PORT', 31042),
                        username="admin", password="passwd")
     except (BadHostKeyException, AuthenticationException,
             SSHException):
